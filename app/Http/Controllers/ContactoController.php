f61c1d28a356d4e89110efc917394294cadf19a0
Lui24 <gnu936@gmail.com>
2024-06-29 21:57:06 -0500

    Add contact form to contacto.blade.php and create route for form submission

---
+++ app/Http/Controllers/ContactoController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\MensajeRecibido;
+
+class ContactoController extends Controller
+{
+    public function store(){
+        $mensaje = request()->validate([
+            'nombre' => 'required',
+            'email' => 'required|email',
+            'asunto' => 'required',
+            'mensaje' => 'required|min:3',
+        ],[
+            'nombre.required' => 'Ingresa tu nombre',
+            'email.required' => 'Ingresa tu email',
+            'asunto.required' => 'Ingresa un asunto',
+            'mensaje.required' => 'Ingresa el mensaje',
+
+        ]);
+        Mail::to('luno2402@gmail.com')->queue(new MensajeRecibido($mensaje));
+        return new MensajeRecibido($mensaje);
+        return 'Datos validados';
+    }
+}
--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -4,4 +4,20 @@
 
 @section('content')
     <h1 class="text-center text-3xl mt-5">Contacto</h1>
+    <form action="{{ route('contacto') }}" method="post">
+        @csrf
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" class="block w-full mt-1" placeholder="Nombre" value="{{ old('nombre') }}">
+        {{ $errors->first('nombre') }}
+        <label for="email">Correo:</label>
+        <input type="email" name="email" class="block w-full mt-1" placeholder="Email" value="{{ old('email') }}">
+        {{ $errors->first('correo') }}
+        <label for="asunto">Asunto:</label>
+        <input type="text" name="asunto" class="block w-full mt-1" placeholder="Asunto" value="{{ old('asunto') }}">
+        {{ $errors->first('asunto') }}
+        <label for="mensaje">Mensaje:</label>
+        <textarea name="mensaje" class="block w-full mt-1" placeholder="Mensaje" value="{{ old('mensaje') }}"></textarea>
+        {{ $errors->first('mensaje') }}
+        <button type="submit" class="block w-full mt-5 bg-blue-500 text-white p-3 rounded">Enviar</button>
+    </form>
 @endsection
